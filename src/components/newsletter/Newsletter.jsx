5da7f69a1b5dcb4c3fec3da62ebb80a8dd0474f1
eimdadul <eimdadulhaque@gmail.com>
2022-01-20 12:56:12 +0600

    newsletter added

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Send } from "@material-ui/icons";
+import "./Newsletter.css";
 
 export default function Newsletter() {
     return (
         <>
             <div className='news-container'>
+                <h1 className='news-title'>
+                    NewsLetter
+                </h1>
                 <div className='news-description'>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, debitis?
                 </div>
                 <div className='news-input-container'>
-                    <input placeholder='Your Email' />
-                    <button>
-                        <Send />
+                    <input className='news-input' placeholder='Your Email' />
+                    <button className='news-btn'>
+                        <Send className='news-icon' />
                     </button>
                 </div>
             </div>
