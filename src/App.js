9f0b556e92af72cac5979cfa43da6be6a5048505
eimdadul <eimdadulhaque@gmail.com>
2022-02-05 14:30:33 +0600

    cart page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React from 'react';
-import Login from './pages/login/Login';
+import Cart from './pages/cart/Cart';
 function App() {
   return (
     <>
-      <Login />
+      <Cart />
     </>
   );
 }
